77fc10c7f5e00aca0aa9fd18bb5ba2a055ab3ddc
Alex Marcos Gutierrez <alexsbd1@gmail.com>
2019-08-07 18:10:54 +0200

    fix(slider): change slider

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,45 +1,46 @@
-import React, { useState, useRef, useEffect } from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import React, { useState, useEffect } from 'react';
+import 'swiper/dist/css/swiper.min.css';
+import 'react-id-swiper/lib/styles/scss/swiper.scss';
+
+import Swiper from 'react-id-swiper';
 import ItemComic from '../../components/ItemComic';
 import './index.scss';
 
 function Carousel({ items, initSlide, onSlideChange, onClickItemComic }) {
   const speedTransition = 700;
-  const carousel = useRef(null);
+  const [swiper, setSwiper] = useState();
   const [currentSlide, setCurrentSlide] = useState(initSlide);
   const carouselItems = items.items.map((val, i) => {
     return (
-      <ItemComic
-        className={i === currentSlide ? 'active' : ''}
-        onClick={() => handleClickItemComic(val, i)}
-        key={i}
-        comic={val}
-      ></ItemComic>
+      <div className="carousel-slide" key={i}>
+        <ItemComic onClick={() => handleClickItemComic(val, i)} comic={val}></ItemComic>
+      </div>
     );
   });
 
   useEffect(() => {
-    carousel.current.slickGoTo(currentSlide, true);
-  }, []);
+    console.log(swiper);
+    if (swiper) {
+      swiper.on('slideChange', handleSlideChange);
+    }
+  }, [swiper]);
 
   const handleClickItemComic = (comic, i) => {
+    const currentSlide = swiper.activeIndex;
     const current = items.items[currentSlide];
     if (comic.id === current.id) {
       if (onClickItemComic) {
         onClickItemComic(comic);
       }
     } else {
-      carousel.current.slickGoTo(i);
-      setTimeout(() => {
-        onClickItemComic(comic);
-      }, speedTransition);
+      swiper.slideTo(i);
+      setTimeout(() => onClickItemComic(comic), speedTransition);
     }
     setCurrentSlide(i);
   };
 
-  const handleSlideChange = next => {
+  const handleSlideChange = () => {
+    const next = swiper.activeIndex;
     if (onSlideChange) {
       onSlideChange(next);
     }
@@ -47,20 +48,26 @@ function Carousel({ items, initSlide, onSlideChange, onClickItemComic }) {
   };
 
   const settings = {
-    className: 'slider',
-    dots: false,
-    infinite: false,
-    speed: speedTransition,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    beforeChange: (current, next) => handleSlideChange(next)
+    effect: 'coverflow',
+    grabCursor: false,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    initialSlide: currentSlide,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: false
+    },
+    shouldSwiperUpdate: true,
+    lazy: true
   };
 
   return (
-    <Slider ref={carousel} {...settings}>
+    <Swiper {...settings} getSwiper={setSwiper}>
       {carouselItems}
-    </Slider>
+    </Swiper>
   );
 }
 
