d23b318067d9744beb2402cb860dd72d8ae7e392
Alex Marcos Gutierrez <alexsbd1@gmail.com>
2019-08-11 19:58:37 +0200

    fix(images): http to https

--- src/services/Adapter.js
+++ src/services/Adapter.js
@@ -4,7 +4,9 @@ class Adapter {
       id: data.id,
       title: data.title,
       description: data.description,
-      thumbnail: `${data.thumbnail.path}/portrait_uncanny.${data.thumbnail.extension}`,
+      thumbnail: `${data.thumbnail.path.replace('http', 'https')}/portrait_uncanny.${
+        data.thumbnail.extension
+      }`,
       prices: data.prices.filter(v => v.type === 'printPrice')[0],
       dates: data.dates.filter(v => v.type === 'onsaleDate')[0],
       creators: data.creators.items.map(creator => {
