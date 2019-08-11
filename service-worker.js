3f53dcc27019765a7231ccc0669c496d616b9de9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-11 09:22:32 +0000

    Deploy kappys1/react-marvel-comics to github.com/kappys1/react-marvel-comics.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3ee45afb33ca040ccffac9f9a4073fc2.js"
+  "/react-marvel-comics/precache-manifest.6ee903915fc2a2ff9e392a9dea58a288.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-marvel-comics/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
