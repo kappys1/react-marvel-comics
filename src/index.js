6406447b186e92878b6dc419e364d7152e8c1f1c
Alex Marcos Gutierrez <alexsbd1@gmail.com>
2019-08-18 12:59:47 +0200

    feat(sw): service worker to cache app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import Home from './pages/Home';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './sw/serviceWorker';
 import configureStore, { history } from './store';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
@@ -22,4 +22,4 @@ ReactDOM.render(
 // If you want your Home to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
