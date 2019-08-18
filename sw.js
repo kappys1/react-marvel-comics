2262213f570dbd54e6c891959963021e42ec2850
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-18 12:14:17 +0000

    Deploy kappys1/react-marvel-comics to github.com/kappys1/react-marvel-comics.git:gh-pages

--- sw.js
+++ sw.js
@@ -8,15 +8,15 @@ if ('function' === typeof importScripts) {
     workbox.precaching.precacheAndRoute([
   {
     "url": "index.html",
-    "revision": "25c3201bcf69119a199a8bbc52ebba8b"
+    "revision": "3120bf11e9c2bd368cdc6a380f5af1fe"
   },
   {
-    "url": "precache-manifest.396ae0c3d6467eef52de06b5379bb231.js",
-    "revision": "396ae0c3d6467eef52de06b5379bb231"
+    "url": "precache-manifest.768580e55aaf94ae02a7add0ee9938a4.js",
+    "revision": "768580e55aaf94ae02a7add0ee9938a4"
   },
   {
     "url": "service-worker.js",
-    "revision": "252c903014b945a48d379a5ce439a12e"
+    "revision": "7723bde2a42410997332f37ea0d5f836"
   },
   {
     "url": "static/css/2.7d2f61d3.chunk.css",
@@ -27,8 +27,8 @@ if ('function' === typeof importScripts) {
     "revision": "cd57a6ff569a3d441ef33fe4ca892db6"
   },
   {
-    "url": "static/css/main.e85b95ac.chunk.css",
-    "revision": "7329657493021b1824a1016ce8590c5a"
+    "url": "static/css/main.a42efc9d.chunk.css",
+    "revision": "770773f28173ac3568492de6b3008bdf"
   },
   {
     "url": "static/js/2.6efc3984.chunk.js",
@@ -39,20 +39,12 @@ if ('function' === typeof importScripts) {
     "revision": "2cd71e68ff23d46f6c04038df7f0cff5"
   },
   {
-    "url": "static/js/main.dbf90fdd.chunk.js",
-    "revision": "1c14404b39b5c1e204fd9999a38fc708"
+    "url": "static/js/main.7f1fa1b1.chunk.js",
+    "revision": "02bab1e700580ab0361b1e7a3b35b288"
   },
   {
     "url": "static/js/runtime~main.f703f9db.js",
     "revision": "8a79b480cd10aab3d80a4928dd945e57"
-  },
-  {
-    "url": "static/media/background.672368c6.png",
-    "revision": "672368c62d0d0c80c271b25cb4c8ba3a"
-  },
-  {
-    "url": "static/media/background2.03addc0b.png",
-    "revision": "03addc0b375fb49057c28f7d3005bdec"
   },
   {
     "url": "static/media/logo.ee0b56fe.png",
@@ -66,15 +58,29 @@ if ('function' === typeof importScripts) {
     });
 
     workbox.routing.registerRoute(
-      /\.(?:png|gif|jpg|jpeg)$/,
+      /.*\.(?:png|jpg|jpeg|svg|gif)/,
       workbox.strategies.cacheFirst({
         cacheName: 'images',
         plugins: [
           new workbox.expiration.Plugin({
             maxEntries: 60,
-            maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+            // Automatically cleanup if quota is exceeded.
+            purgeOnQuotaError: true
           })
         ]
+      })
+    );
+
+    workbox.router.registerRoute(
+      process.env.REACT_APP_API_ROUTER,
+      workbox.strategies.cacheFirst({
+        cacheName: 'apiCache',
+        cacheExpiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 3600 // 60 minutes
+        },
+        cacheableResponse: { statuses: [0, 200] }
       })
     );
   } else {
