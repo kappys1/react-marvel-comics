23d067a809fa9fcccdc1af9219a9a18a409d5985
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-20 10:08:25 +0000

    Deploy kappys1/react-marvel-comics to github.com/kappys1/react-marvel-comics.git:gh-pages

--- sw.js
+++ sw.js
@@ -9,15 +9,15 @@ if ('function' === typeof importScripts) {
     workbox.precaching.precacheAndRoute([
   {
     "url": "index.html",
-    "revision": "58eb12c2604d91d5dc951b8728058fdb"
+    "revision": "5f0c2e8cd5191d5de1a485e366c05bad"
   },
   {
-    "url": "precache-manifest.e9a2741158f5ce103c3095cb79dc8028.js",
-    "revision": "e9a2741158f5ce103c3095cb79dc8028"
+    "url": "precache-manifest.9d3ccaa7d50b99d64114f88591d99dba.js",
+    "revision": "9d3ccaa7d50b99d64114f88591d99dba"
   },
   {
     "url": "service-worker.js",
-    "revision": "c488796ad4e39f34591d010ebf68b5f5"
+    "revision": "0d92f6e10350e2ee7620d48996e44970"
   },
   {
     "url": "static/css/2.3f3d6e18.chunk.css",
@@ -28,8 +28,8 @@ if ('function' === typeof importScripts) {
     "revision": "57d1892a13915eca12deffcb19221043"
   },
   {
-    "url": "static/css/main.648627ae.chunk.css",
-    "revision": "0cc56bea0eb0350e4e0b3b38dbe8077c"
+    "url": "static/css/main.b68cad30.chunk.css",
+    "revision": "f9fda29cd7d03766525947ccec467b97"
   },
   {
     "url": "static/js/2.0c766980.chunk.js",
@@ -40,12 +40,12 @@ if ('function' === typeof importScripts) {
     "revision": "937353d4ec16d24f027686568b6c434a"
   },
   {
-    "url": "static/js/main.7c1ee568.chunk.js",
-    "revision": "f18b79beea5a5e0fb0dac3bdcf4273aa"
+    "url": "static/js/main.6d82a2fe.chunk.js",
+    "revision": "0758650c722e1ab481e0e9c66038dd92"
   },
   {
-    "url": "static/js/runtime~main.3d697109.js",
-    "revision": "4bb268cce3606299fab6b3f0fb81cd14"
+    "url": "static/js/runtime~main.a70fc7bd.js",
+    "revision": "8e5d479d9930cb4628c9921548611a57"
   },
   {
     "url": "static/media/logo.ee0b56fe.png",
